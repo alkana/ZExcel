c8834cd55c717d6e4cd9efd676ab0864250aa697
Rémi Dreillard <totos18@msn.com>
2016-03-07 19:48:37 +0000

    Clean up unitTest folder

--- unitTests/bootstrap.php
+++ unitTests/bootstrap.php
@@ -15,11 +15,31 @@ setlocale(LC_ALL, 'en_US.utf8');
 // PHP 5.3 Compat
 date_default_timezone_set('Europe/London');
 
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../Classes'));
+
+// Define path to application tests directory
+defined('APPLICATION_TESTS_PATH')
+    || define('APPLICATION_TESTS_PATH', realpath(dirname(__FILE__)));
+
+// Define application environment
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'ci');
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../Classes'),
+    './',
+    dirname(__FILE__),
+    get_include_path(),
+)));
+
+
 /**
  * @todo Sort out xdebug in vagrant so that this works in all sandboxes
  * For now, it is safer to test for it rather then remove it.
  */
-echo "ZExcel tests beginning\n";
+echo "PHPExcel tests beginning\n";
 
 if (extension_loaded('xdebug')) {
     echo "Xdebug extension loaded and running\n";
